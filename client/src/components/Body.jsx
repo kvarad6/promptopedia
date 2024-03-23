5f597ebce5f444731a1f105e5d0127431336a0b3
varad.kulkarni <kvarad6@gmail.com>
2024-03-23 15:25:13 +0530

    added components | jsx

---
+++ client/src/components/Body.jsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import SearchBar from './SearchBar'
+
+const Body = () => {
+    return (
+        <div>
+            <Typography>Discover & Share AI-Powered Prompts</Typography>
+            <Typography>Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</Typography>
+            <SearchBar />
+        </div>
+    )
+}
+
+export default Body
